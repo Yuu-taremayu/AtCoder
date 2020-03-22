d88db630648477663ccf0ec8c0fc25fe31552934
Yuu-taremayu <taremayu.yuu@gmail.com>
2020-03-22 07:05:04 +0000

    if文・比較演算子・論理演算子

---
+++ APG4b/G.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	string op;
+
+	cin >> a >> op >> b;
+
+	if (op == "+") {
+		cout << a + b << endl;
+	}
+	else if (op == "-") {
+		cout << a - b << endl;
+	}
+	else if (op == "*") {
+		cout << a * b << endl;
+	}
+	else if (op == "/") {
+		if (b == 0) {
+			cout << "error" << endl;
+		}
+		else {
+			cout << a / b << endl;
+		}
+	}
+	else {
+		cout << "error" << endl;
+	}
+
+	return 0;
+}
