1fe14971e519d9bf73c236ca98f9c2c5a26dcf2f
Yuu-taremayu <taremayu.yuu@gmail.com>
2020-04-05 10:38:52 +0000

    解説見て解いた

--- ABC161/D_LunlunNumber.cpp
+++ ABC161/D_LunlunNumber.cpp
@@ -1,43 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool judge_abs(int a, int b)
-{
-	if (abs(a - b) <= 1) return true;
-	else return false;
-}
-
-void increment_func(vector<int> &A)
-{
-	for (int i = 99; i > 1; i--) {
-		if (A.at(i) == 9) {
-			A.at(i
-		if (A.at(i) == 0) {
-			A.at(i) = A.at(i) + 1;
-			break;
-		}
-		else {
-			A.at(i) = (A.at(i) + 1) % 10;
-			break;
-		}
-	}
-}
-
 int main(void)
 {
 	int K;
-
 	cin >> K;
 
-	vector<int> A(100, 0);
+	queue<int> q;
 
-	for (int i = 0; i < 100; i++) {
-		increment_func(A);
+	for (int i = 1; i < 10; i++) {
+		q.push(i);
 	}
 
-	for (int i = 0; i < 100; i++) {
-		cout << A.at(i);
+	int x = 0;
+	for (int i = 0; i < K; i++) {
+		x = q.front();
+		q.pop();
+		if (x % 10 != 0) {
+			q.push(10 * x + x % 10 - 1);
+		}
+		q.push(10 * x + x % 10);
+		if (x % 10 != 9) {
+			q.push(10 * x + x % 10 + 1);
+		}
 	}
-	cout << endl;
+
+	cout << x << endl;
+
 	return 0;
 }
